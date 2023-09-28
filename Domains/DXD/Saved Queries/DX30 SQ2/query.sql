56e0a23acd542f16c94a2cfbd0d2327d978d43e5
Bhuvaneshwari Sirsabesan <114596096+BhuvanaSabi@users.noreply.github.com>
2023-09-28 13:43:33 -0400

    Add files via upload
    Added EX files exported  using rc6

---
+++ Domains/DXD/Saved Queries/DX30 SQ2/query.sql
@@ -0,0 +1,4 @@
+SELECT [Cinchy Id], [Created],[Text], [Bool], [Calculated], [Choice], [Date], [Link], [Multi Select Choice], [Number]
+FROM [QA].[DX30 All Data Types Link Target]
+WHERE [Deleted] IS NULL
+AND [CINCHY Id] < 100
