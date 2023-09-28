790aa4efb61dc196f2187dfd4d17616d15823db1
Bhuvana Sabi <bhuvaneshwari.sirsabesan@cinchy.com>
2023-09-28 13:39:14 -0400

    Revert "Add files via upload"
    This reverts commit aca4f461f1030907aa7927eb17e30255d4b7087a.

--- Domains/DXD/Saved Queries/DX30 SQ1/query.sql
+++
@@ -1,3 +0,0 @@
-SELECT [Cinchy Id],[Text]
-FROM [QA].[Nested DX30 Link Target]
-WHERE [Deleted] IS NULL
--- Domains/DXD/Saved Queries/DX30 SQ2/query.sql
+++
@@ -1,4 +0,0 @@
-SELECT [Cinchy Id], [Created],[Text], [Bool], [Calculated], [Choice], [Date], [Link], [Multi Select Choice], [Number]
-FROM [QA].[DX30 All Data Types Link Target]
-WHERE [Deleted] IS NULL
-AND [CINCHY Id] < 100
